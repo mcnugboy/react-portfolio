c53980e5db727f2e7bec11c16857215a5147cacb
space cadet grant <53055825+mcnugboy@users.noreply.github.com>
2022-09-28 17:35:08 -0500

    Added about me jsx with cards & icons.

--- r-port/src/components/about/About.jsx
+++ r-port/src/components/about/About.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
 import './about.css'
+// import ME from ''
+import {FaCode, FaGamepad} from 'react-icons/fa'
+import {MdEngineering} from 'react-icons/md'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Learn</h5>
+      <h2>About Me</h2>
+
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me-image">
+            {/* <img src={ME} alt='' /> */}
+          </div>
+        </div>
+
+        <div className="about_container">
+          <div className="about_cards">
+            <article className='about_card'>
+              <FaCode className='about_icon' />
+              <h5>Development</h5>
+              <small>2+ Years Knowledge</small>
+            </article>
+
+            <article className='about_card'>
+              <MdEngineering className='about_icon' />
+              <h5>Engineering</h5>
+              <small>4+ Years On-Site Experience</small>
+            </article>
+
+            <article className='about_card'>
+              <FaGamepad className='about_icon' />
+              <h5>Gaming</h5>
+              <small>300+ Games Played-Thru</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, eaque magni. Dolores accusamus, nulla itaque perspiciatis maxime repudiandae facilis corrupti natus minus tempore voluptatibus reprehenderit ab consequatur velit tenetur ipsum.
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Lets chat it up!</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- r-port/src/components/nav/Nav.jsx
+++ r-port/src/components/nav/Nav.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './nav.css'
+import {HiHome, HiUser, HiChatAlt} from 'react-icons/hi'
+import {BsBookmarkFill} from 'react-icons/bs'
+import {useState} from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><HiHome/></a>
+      <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><HiUser/></a>
+      <a href='#experience' onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BsBookmarkFill/></a>
+      <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><HiChatAlt/></a>
+    </nav>
   )
 }
 
