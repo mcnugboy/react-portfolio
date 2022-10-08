5d3312dc7d2b015e26cf593257acb9392cecf11d
space cadet grant <53055825+mcnugboy@users.noreply.github.com>
2022-10-08 12:06:05 -0500

    Added exp.

--- r-port/src/components/experience/Experience.jsx
+++ r-port/src/components/experience/Experience.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
 import './experience.css'
+// import '' from 'react-icons/'
+
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h2>Experience</h2>
+
+      <div className='container experience_container'>
+        <div className='experience_cards'>
+          <article className='experience_card'>
+            <h5>Knowledge In...</h5>
+            <div className='experience-list'>
+              <ul>
+                <li><h6>HTML</h6></li>
+                <li><h6>CSS</h6></li>
+                <li><h6>Git</h6></li>
+                <li><h6>JavaScript</h6></li>
+                <li><h6>SQL</h6></li>
+                <li><h6>NoSQL</h6></li>
+                <li><h6>MongoDB & Mongoose</h6></li>
+                <li><h6>Node.js</h6></li>
+                <li><h6>Express.js</h6></li>
+                <li><h6>React</h6></li>
+                <li><h6>WebAPIs</h6></li>
+                <li><h6>FANUC Robot Programming</h6></li>
+                <li><h6>SIMATIC S7</h6></li>
+                <li><h6>Active Directory</h6></li>
+                <li><h6>Imaging Machines</h6></li>
+                <li><h6>Networking</h6></li>
+                <li><h6>Blockchain Development</h6></li>
+                <li><h6>InfoSec & Tech</h6></li>
+                <li><h6>Machining & Computing Hardware</h6></li>
+              </ul>
+            </div>
+          </article>
+        </div>
+      </div>
+      </section>
   )
 }
 
